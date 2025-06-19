b2fa95541456a57d9d434dfbae829882272390a0
yanghaetsal2 <yanghaetsal2>
2025-06-19 08:36:28 +0000

    pulled
    pulled

--- notebook.py
+++ notebook.py
@@ -4,5 +4,5 @@ print("Hello from Databricks notebook!")
 # COMMAND ----------
 # 두 번째 셀 코드
 import pandas as pd
-df = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+df = pd.DataFrame({'col1': [1, 2], 'col2': [2, 4]})
 print(df)
