0863d39c25f5cfbbd40242cf4f5bcc911c3f96a9
YANG HAETSAL <hsyang@zenkoh.com>
2025-06-19 14:09:11 +0900

    Create notebook.py

---
+++ notebook.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+
+# COMMAND ----------
+# 첫 번째 셀 코드
+print("Hello from Databricks notebook!")
+
+# COMMAND ----------
+# 두 번째 셀 코드
+import pandas as pd
+df = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+print(df)
